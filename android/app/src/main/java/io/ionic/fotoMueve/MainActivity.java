6968dccf0bb4167f55726c84d49f7d7284bbb821
Belen Paiz <belen.paiz.220686@gmail.com>
2024-10-24 01:00:14 -0300

    ADD: splash - icono - login

---
+++ android/app/src/main/java/io/ionic/fotoMueve/MainActivity.java
@@ -0,0 +1,5 @@
+package io.ionic.fotoMueve;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.fotoMueve',
+  appName: 'fotoMueve',
+  webDir: 'www'
+};
+
+export default config;
